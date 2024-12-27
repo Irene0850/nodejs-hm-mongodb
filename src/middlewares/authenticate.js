f362748e947c876838a46c4d006c619c580dc4da
Iryna Kolomiichenko <159377221+Irene0850@users.noreply.github.com>
2024-12-27 23:06:20 +0200

    new

--- src/middlewares/authenticate.js
+++ src/middlewares/authenticate.js
@@ -1,6 +1,7 @@
 import createHttpError from 'http-errors';
-import { Session } from '../db/models/Session.js';
+
 import { UsersCollection } from '../db/models/User.js';
+import { Session } from '../db/models/Session.js';
 
 export const authenticate = async (req, res, next) => {
   const authHeader = req.get('Authorization');
