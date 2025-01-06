278b56f474ef6c80bedad557c09638b52fd5a219
Iryna Kolomiichenko <159377221+Irene0850@users.noreply.github.com>
2025-01-06 21:16:03 +0200

    new

--- src/server.js
+++ src/server.js
@@ -7,6 +7,7 @@ import cookieParser from 'cookie-parser';
 import router from './routers/index.js';
 import { env } from './utils/env.js';
 import { ENV_VARS, UPLOAD_DIR } from './contacts/index.js';
+import { swaggerDocs } from './middlewares/swaggerDocs.js';
 
 const PORT = Number(env(ENV_VARS.PORT, '3001'));
 
@@ -37,9 +38,10 @@ export const setupServer = () => {
   });
 
   app.use(router);
+  app.use('/uploads', express.static(UPLOAD_DIR));
+  app.use('/api-docs', swaggerDocs());
   app.use('*', notFoundHandler);
   app.use(errorHandler);
-  app.use('/uploads', express.static(UPLOAD_DIR));
 
   app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
